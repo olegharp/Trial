2089db9ed50fc61512b6c58c8c15c5ba57fe2757
olegharp <px845pev@mail.ru>
2018-03-03 23:13:15 +0300

    Конфликт

--- Sharp/Program.cs
+++ Sharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Sharp
         [STAThread]
         static void Main()
         {
+            // конфликт
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
